4a21d2a36da6e40ddd6ca2124edd48fd3731f76d
aahik66 <aahik66@users.noreply.github.com>
2026-09-14 15:33:14 +0600

    Live auto-sync update [9/14/2026 3:33:14 PM]

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Logo from "../components/Logo";
 
 export default function Register() {
   const { register, loading, isAuthenticated } = useAuth();
