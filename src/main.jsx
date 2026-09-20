ab27630b62230c1583bc1987c20deac8e1a3201e
aahik66 <aahik66@users.noreply.github.com>
2026-09-20 14:48:44 +0600

    Add runtime gtag setup in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { SettingsProvider } from "./context/SettingsContext";
 
+// Initialize Google Tag dynamically
+if (typeof window !== "undefined") {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+  window.gtag("js", new Date());
+  window.gtag("config", "G-FRZD7DRX0M");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
